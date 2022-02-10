17a9a783995d82965f5dda8d4e757ae02b38bc1b
aaaaargZombies <ahberate@riseup.net>
2022-02-10 11:51:56 +0000

    improve layout of orgs

--- .eleventy.js
+++ .eleventy.js
@@ -25,21 +25,44 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addShortcode("organisation", function(collectionObject) {
     return outdent`
-      <h2>${collectionObject.data.title}</h2>
+      <h2 class="org__title">${collectionObject.data.title}</h2>
+      <ul role="list" class="org__contact"]>
+        <li class="org__contact__method">
+          <p class="org__contact__method__type" >Phone</p>
+          <a href="tel:${collectionObject.data.tel}">${collectionObject.data.tel}</a></li>
+        <li class="org__contact__method">
+          <p class="org__contact__method__type" >Email</p>
+          <a href="mailto:${collectionObject.data.email}">${collectionObject.data.email}</a></li>
+        <li class="org__contact__method">
+          <p class="org__contact__method__type" >Website</p>
+          <a href="${collectionObject.data.web}">${collectionObject.data.web}</a></li>
+      </ul>
       ${collectionObject.templateContent}
-      <p>TEL: <a href="tel:${
-        collectionObject.data.tel
-      }">${collectionObject.data.tel}</a></p>
-      <p><a href="${collectionObject.data.web}">Website</a></p>
-      <ul>
-        ${collectionObject.data.tags
-          .map(
-            (tag) =>
-              `<li><a href="/orgs/${slugify(
-                tag.toLowerCase(),
-              )}">${tag}</a></li>`,
-          )
-          .join("")}
+    `;
+  });
+
+  eleventyConfig.addShortcode("catagories", function(tagArray, all = false) {
+    tagArray = tagArray.filter((tag) => tag !== "orgs");
+    return outdent`
+      <ul role="list" class="catagories">
+          ${
+            all
+              ? `<li class="catagorie">
+              <a class="catagorie__link" href="/pages/who/">All</a>
+          </li>`
+              : ""
+          }
+          ${tagArray
+            .map(
+              (tag) =>
+                `<li class="catagorie">
+                <a class="catagorie__link" href="/orgs/${slugify(
+                  tag.toLowerCase(),
+                )}">${tag}</a>
+            </li>`,
+            )
+            .join("")}
+
       </ul>
     `;
   });
