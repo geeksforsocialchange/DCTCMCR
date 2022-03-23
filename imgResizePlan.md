f1ee5228694dde291e16ad357c3121f086390bfe
aaaaargZombies <ahberate@riseup.net>
2022-03-23 15:23:19 +0000

    👷WIP👷

---
+++ imgResizePlan.md
@@ -0,0 +1,22 @@
+# Plan
+
+Automatically resixe images at build without needing a shortcode or any extra steps for the user
+
+## Phase 1
+
+make a barebones node-script
+
+This sort of works see
+
+```
+in.html
+out.html
+mvp.js
+```
+
+# Phase 2
+
+- use https://www.11ty.dev/docs/config/#transforms & https://www.11ty.dev/docs/plugins/image/
+- run through the compiled html, look for img tags and pass the urls through eleventy-img
+
+**Question** will running over the same url dump millions of new files into the public folder each time????
---
+++ mvp.js
@@ -0,0 +1,41 @@
+const { readFile, writeFile } = require("fs/promises");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const Image = require("@11ty/eleventy-img");
+
+// STATS OBJECT
+// {
+//   jpeg: [
+//     {
+//       format: "jpeg",
+//       width: 900,
+//       height: 1500,
+//       url: "/img/lZ-pVb18oK-900.jpeg",
+//       sourceType: "image/jpeg",
+//       srcset: "/img/lZ-pVb18oK-900.jpeg 900w",
+//       filename: "lZ-pVb18oK-900.jpeg",
+//       outputPath: "public/img/lZ-pVb18oK-900.jpeg",
+//       size: 114405,
+//     },
+//   ];
+// }
+
+const resizer = async (url) => {
+  let stats = await Image(url, {
+    outputDir: "./public/img/",
+    formats: ["jpeg"],
+    widths: [900],
+  });
+
+  return stats.jpeg[0].url;
+};
+
+resizer("public/img/phone.png");
+readFile("in.html", { encoding: "utf-8" }).then(async (contents) => {
+  const dom = new JSDOM(contents);
+  src = dom.window.document.querySelector("img").src;
+  dom.window.document.querySelector("img").src = `public/${await resizer(src)}`;
+  console.log(src);
+  console.log(dom.window.document.documentElement.outerHTML);
+  writeFile("out.html", dom.window.document.documentElement.outerHTML);
+});
